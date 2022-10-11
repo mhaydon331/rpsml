b935e10f809b650356c606e424e24a9eb62c89fd
mhaydon331 <70169970+mhaydon331@users.noreply.github.com>
2022-10-11 15:25:54 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# This entrypoint file to be used in development. Start by reading README.md
 from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
 from RPS import player
 from unittest import main
